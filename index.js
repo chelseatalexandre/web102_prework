b140ebeed5d6db70314b68f07ee3b0752982d006
Chelsea Alexandre <158171651+chelseatalexandre@users.noreply.github.com>
2026-05-18 01:12:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const gamesContainer = document.getElementById("games-container");
 
     function addGamesToPage(games) {
-
         gamesContainer.innerHTML = "";
 
         for (const game of games) {
-
             const gameCard = document.createElement("div");
-
             gameCard.classList.add("game-card");
 
             gameCard.innerHTML = `
@@ -28,30 +25,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
     addGamesToPage(GAMES_JSON);
 
+    const contributionsCard = document.getElementById("num-contributions");
+    const raisedCard = document.getElementById("total-raised");
+    const gamesCard = document.getElementById("num-games");
+
+    const contributions = GAMES_JSON.reduce((acc, game) => acc + game.backers, 0);
+    const raised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0);
+
+    contributionsCard.innerHTML = contributions.toLocaleString("en-US");
+    raisedCard.innerHTML = "$" + raised.toLocaleString("en-US");
+    gamesCard.innerHTML = GAMES_JSON.length;
+
     const unfundedBtn = document.getElementById("unfunded-btn");
     const fundedBtn = document.getElementById("funded-btn");
     const allBtn = document.getElementById("all-btn");
 
     unfundedBtn.addEventListener("click", () => {
-
-        const unfundedGames = GAMES_JSON.filter(game => {
-            return game.pledged < game.goal;
-        });
-
+        const unfundedGames = GAMES_JSON.filter(game => game.pledged < game.goal);
         addGamesToPage(unfundedGames);
     });
 
     fundedBtn.addEventListener("click", () => {
-
-        const fundedGames = GAMES_JSON.filter(game => {
-            return game.pledged >= game.goal;
-        });
-
+        const fundedGames = GAMES_JSON.filter(game => game.pledged >= game.goal);
         addGamesToPage(fundedGames);
     });
 
     allBtn.addEventListener("click", () => {
         addGamesToPage(GAMES_JSON);
     });
+
+    const firstGameContainer = document.getElementById("first-game");
+    const secondGameContainer = document.getElementById("second-game");
+
+    const sortedGames = [...GAMES_JSON].sort((a, b) => b.pledged - a.pledged);
+    const [firstGame, secondGame] = sortedGames;
+
+    firstGameContainer.innerHTML = `<p>${firstGame.name}</p>`;
+    secondGameContainer.innerHTML = `<p>${secondGame.name}</p>`;
 
 });
