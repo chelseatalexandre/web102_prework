21b6c09cbf3aa7da17000b2031d49f27b84b8455
Chelsea Alexandre <158171651+chelseatalexandre@users.noreply.github.com>
2026-05-18 00:54:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,25 @@ import GAMES_DATA from './games.js';
 
 const GAMES_JSON = JSON.parse(GAMES_DATA);
 
-function deleteChildElements(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+document.addEventListener("DOMContentLoaded", () => {
+    const gamesContainer = document.getElementById("games-container");
+
+    function addGamesToPage(games) {
+        gamesContainer.innerHTML = "";
+
+        for (const game of games) {
+            const gameCard = document.createElement("div");
+            gameCard.classList.add("game-card");
+
+            gameCard.innerHTML = `
+                <h3>${game.name}</h3>
+                <p>${game.description}</p>
+                <p>Pledged: $${game.pledged}</p>
+            `;
+
+            gamesContainer.appendChild(gameCard);
+        }
     }
-}
 
-const gamesContainer = document.getElementById("games-container");
-
-function addGamesToPage(games) {
-    for (const game of games) {
-        const gameCard = document.createElement("div");
-
-        gameCard.classList.add("game-card");
-
-        gameCard.innerHTML = `
-            <img class="game-img" src="${game.img}" />
-            <h3>${game.name}</h3>
-            <p>${game.description}</p>
-            <p>Pledged: $${game.pledged}</p>
-        `;
-
-        gamesContainer.appendChild(gameCard);
-    }
-}
-
-addGamesToPage(GAMES_JSON);
+    addGamesToPage(GAMES_JSON);
+});
